6a898751478198f690bc5b0dcb83046d53b2b6e1
Trey Mosley <iiimosley@Treys-MacBook-Pro.local>
2017-11-27 14:03:14 -0600

    initial js

--- js/products.js
+++ js/products.js
@@ -1,28 +1,64 @@
-var 
+var productCard = document.getElementById(products.id[i]).innerHTML
 
 var products = [
     {
-
+        id: "star1",
+        name: "Nebula #415",
+        price: "$45,600",
+        description: ,
+        image: ,
     },
     {
-
+        id: "star2",
+        name: "Red Giant #7865",
+        price: "$567,800",
+        description: ,
+        image: ,
     },
     {
-
+        id: "star3",
+        name: "SuperNova #336",
+        price: "$10,928,366",
+        description: ,
+        image: ,
     },
     {
-        
+        id: "star4",
+        name: "White Dwarf #24567",
+        price: "$2",
+        description: ,
+        image: ,
     },
     {
-        
+        id: "star5",
+        name: "Nebula #344323",
+        price: "~$789",
+        description: ,
+        image: ,
     },
     {
-        
+        id: "star6",
+        name: "Blackhole 7664",
+        price: "$0",
+        description: ,
+        image: ,
     },
     {
-        
+        id: "star7",
+        name: "Galaxy #45e89",
+        price: "$767",
+        description: ,
+        image: ,
     },
     {
-        
+        id: "star8",
+        name: "Pillars of Creation",
+        price: "$3,000,000,000",
+        description: ,
+        image: ,
     },
 ];
+
+for (var i=0; i<products.length; i++) {
+    productCard+= 
+}
